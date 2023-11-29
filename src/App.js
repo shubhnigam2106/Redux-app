449350d85c115d4963a357c0ae878b4b3486c021
shubhnigam <shubh.nigam@nagarro.com>
2023-11-29 17:40:04 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './Pages/Home';
+import {BrowserRouter as Router,Routes,Route,Switch} from "react-router-dom"
+import Navbar from "./Pages/Header/Navbar"
+import Home from './Pages/Home/Home';
+import Details from './Pages/Movie-Detail/Details';
+import PageNotFound from './Pages/PageNotFound/PageNotFound';
+import Footer from './Pages/Footer/Footer';
 
 function App() {
   return (
-   <>
-<Home/>  
-     </>
+  <div className="app">
+    <Router>
+    <Navbar />
+
+      <Routes>
+      <Route path="/" Component={Home}/>
+      <Route path="/movie/:imdbId" Component={Details}/>
+      <Route  Component={PageNotFound}/>
+      </Routes>  
+          <Footer/>
+
+    </Router>
+  </div>
   );
 }
 
